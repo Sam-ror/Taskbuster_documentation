30b436af1ca0327e86fb99910818e45f28a1b3d2
Sam-ROR <sandeep@rubyeffect.com>
2016-10-20 18:13:36 +0530

    static files and templates with test cases to validate the configurations

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -26,6 +26,10 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -65,7 +69,7 @@ ROOT_URLCONF = 'taskbuster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -87,7 +91,7 @@ WSGI_APPLICATION = 'taskbuster.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_blog',
+        'NAME': 'django_taskbuster',
         'USER': 'root',
         'PASSWORD': 'nimbu79',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
@@ -131,5 +135,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
 
 STATIC_URL = '/static/'
